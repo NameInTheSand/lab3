61cefaa4fed3d9144aed07f3c09b7967101d0dd9
Your Name <semernnaduev@gmail.com>
2020-11-21 13:27:38 +0200

    change list

--- js/app.js
+++ js/app.js
@@ -11,21 +11,16 @@
         $scope.listOfProducts = "";
 
         $scope.showAnswer = function () {
-            $scope.text = calculate($scope.listOfProducts);
-        }
-
-        function calculate(products1) {
-            let products = products1.split(',');
-
+                   let products = $scope.listOfProducts.split(',');
             for (let i = products.length - 1; i >= 0; i--) {
                 if (products[i].trim() === "") products.splice(i, 1);
             }
             if (products.length === 0) {
-                return"Будь ласка, уведіть дані!";
+                 $scope.text = "Будь ласка, уведіть дані!";
             } else if (products.length <= 3) {
-                return "На здоров'я!";
+                 $scope.text = "На здоров'я!";
             } else {
-                return "Це забагато!";
+                 $scope.text = "Це забагато!";
             }
         }
     }
